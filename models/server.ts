266b49de8abb5dfadb2e4ff3022eff140964f944
Juan Alejandro Work <alej1286@gmail.com>
2023-02-20 16:05:11 -0500

    base route and controller created

--- models/server.ts
+++ models/server.ts
@@ -1,10 +1,22 @@
 import express, { Application } from "express";
+import userRoutes  from "../routes/user.route";
 class Server {
   private app: Application;
   private port: string;
+  private apiPaths = {
+    users: "/api/users",
+  };
+
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "8000";
+    this.routes();
+  }
+
+  routes(){
+
+    this.app.use(this.apiPaths.users,userRoutes)
+
   }
 
   listen() {
